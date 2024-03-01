315143e17092d755d10b089bf0bd27611ff3880c
Danilo Aleixo <danilo_aleixo@hotmail.com>
2024-03-01 10:48:43 +0100

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -22,3 +22,8 @@ sudo apt install -y terminator
 # Install indicator multiload 
 sudo apt install indicator-multiload
 
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io

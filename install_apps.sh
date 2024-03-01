4eab3d6995d3935079e81689841bc7b74c90613c
Danilo Aleixo <danilo_aleixo@hotmail.com>
2024-03-01 10:49:26 +0100

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -27,3 +27,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Install xcalib
+sudo apt install xcalib
